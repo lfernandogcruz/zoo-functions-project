a3654b1ffbaa6a1e8bf912f9333f5f639112d815
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-03-16 16:45:04 -0300

    req 1.1 - retorna array vazio caso funcao nao receba parametros

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,6 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
+  // Esta função, a partir do nome de uma espécie e uma idade mínima, verifica se todos os animais daquela espécie possuem a idade mínima especificada.
+  // Observações técnicas
+  // Deve retornar um valor booleano.
+  // O que será avaliado
+  // Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada.
   // seu código aqui
 }
 
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
+  // Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
+  // Observações técnicas
+  // O parâmetro desta função pode ser alterado para atender ao requisito proposto.
+  // O que será avaliado
+  // Caso receba nenhum parâmetro, necessário retornar um array vazio;
+  // Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id;
+  // Ao receber mais de um id, retorna um array com as espécies referentes aos ids.
   // seu código aqui
+  if (ids === '' || ids === undefined) {
+    return [];
+  }
 }
 
 module.exports = getSpeciesByIds;
