b87da076e0f3f11227db5557e92f766b91df3791
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-03-17 01:59:02 -0300

    req 01 - VITORIAgit add src/.! Com a graca de NSAVOCS consegui pelo menos umgit add src/. (NSAVVOCS = Nossa Senhora da Aula ao Vivo de Ontem da Carol Silva)

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,17 @@
-// const { species } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const saida = [];
+  ids.forEach((elementoId) => {
+    species.forEach((elementoEspecie) => {
+      if (elementoEspecie.id === elementoId) {
+        saida.push(elementoEspecie);
+      }
+    });
+  });
+  return saida;
 }
 // console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 // Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
