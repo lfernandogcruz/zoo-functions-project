698ee8dcc4b6d2a99076db8995d7946788097c33
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-03-17 01:28:51 -0300

    commit Avril Lavigne. Complicated

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,28 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+// A função busca por informações do animal mais velho da primeira espécie gerenciada pela pessoa colaboradora do parâmetro.
+
+// O que será avaliado
+
+// Passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie.
+
+// id -> find -> 1a especie -> animal mais velho
+// .............................['nome', 'sexo', idade]
+// species = [
+//   {
+//     id: abubleabuble,
+//     residents: [
+//       {
+//         name: 'Cacareco',
+//         sex: 'male',
+//         age: 43
+//       },
+//     ]
+//   },
+// ]
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,18 +1,36 @@
-const { species } = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
-  // Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
-  // Observações técnicas
-  // O parâmetro desta função pode ser alterado para atender ao requisito proposto.
-  // O que será avaliado
-  // Caso receba nenhum parâmetro, necessário retornar um array vazio;
-  // Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id;
-  // Ao receber mais de um id, retorna um array com as espécies referentes aos ids.
+function getSpeciesByIds(ids) {
   // seu código aqui
-  const tempConst = species.filter((cadaEspecie) => cadaEspecie === ids);
-  console.log(tempConst);
-  return tempConst;
 }
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+// Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
+// Observações técnicas
+// O parâmetro desta função pode ser alterado para atender ao requisito proposto.
+// O que será avaliado
+// Caso receba nenhum parâmetro, necessário retornar um array vazio;
+// Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id;
+// Ao receber mais de um id, retorna um array com as espécies referentes aos ids.
+
+// const saida = [];
+// for (let i = 0; i < ids.length; i += 1) {
+//   for (let j = 0; j < species.length; j += 1) {
+//     if (species[j].id === ids[i][0]) {
+//       saida.push(species[j]);
+//     }
+//   }
+// }
+// return saida;
+
+// const temp = species.forEach((especieNoArray) => {
+//   if (especieNoArray.id === ids) {
+//     saida.push(species[especieNoArray]);
+//   }
+// })
+
+// seu código aqui
+// const tempConst = species.filter((cadaEspecie) => cadaEspecie === ids);
+// return tempConst;
 
 module.exports = getSpeciesByIds;
