55b70ee84abbbf6bfdd9f2f53ee324b74cbcad00
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-03-18 20:12:25 -0300

    Serenity now... Serenity now...

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,12 +1,22 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // Esta função, a partir do nome de uma espécie e uma idade mínima, verifica se todos os animais daquela espécie possuem a idade mínima especificada.
-  // Observações técnicas
-  // Deve retornar um valor booleano.
-  // O que será avaliado
-  // Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada.
   // seu código aqui
+  const { name, residents } = data.species;
+  // find(animal)
+  const tagAnimal = name.find((elementoFind) => elementoFind === animal);
+  console.log('tagged animal:', tagAnimal);
+  // every (idade >= age)
+  const ageCheck = tagAnimal.every((verifier) => tagAnimal.residents.age >= age);
+  console.log('age check:', ageCheck);
+  return ageCheck;
 }
+console.log(getAnimalsOlderThan('lions', 13));
 
 module.exports = getAnimalsOlderThan;
+
+  // Esta função, a partir do nome de uma espécie e uma idade mínima, verifica se todos os animais daquela espécie possuem a idade mínima especificada.
+  // Observações técnicas
+  // - Deve retornar um valor booleano.
+  // O que será avaliado
+  // - Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada.
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,9 +1,19 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
+function getOldestFromFirstSpecies(...id) {
   // seu código aqui
-
+  // const { species } = data;
+  // const firstSpecies = id.reduce((acc, cur) => {
+  //   acc += species.find((elementoFind) => elementoFind === cur[id]);
+  //   return acc;
+  // }, []);
+  // console.log(firstSpecies);
+  // return firstSpecies;
 }
+
+// const teste1 = 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1';
+// const teste2 = '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae';
+// console.log(getOldestFromFirstSpecies(teste1, teste2));
 
 module.exports = getOldestFromFirstSpecies;
 
@@ -12,6 +22,9 @@ module.exports = getOldestFromFirstSpecies;
 // O que será avaliado
 
 // Passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie.
+
+// let firstSpecies = species.filter((filtroDeEspecie) => filtroDeEspecie.id === id.find((encontradorDeIds) => species[encontradorDeIds]));
+// id.find((encontradorDeIds) => encontradorDeIds === filtroDeEspecie.id))
 
 // id -> find -> 1a especie -> animal mais velho
 // .............................['nome', 'sexo', idade]
