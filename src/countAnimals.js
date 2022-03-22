69f1ecf758ea88773d622d884b3a38e4320be36a
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-03-22 02:02:23 -0300

    req 5.1 - param vazio foi, com um a caminho, com dois, ja ja

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,34 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const { species } = data;
+  // blank
+  if (animal === undefined) {
+    const blank = species.reduce((acc, eachSpecies) => {
+      acc[eachSpecies.name] = eachSpecies.residents.length;
+      // console.log(acc);
+      return acc;
+    }, {});
+    return blank;
+  }
 }
 
+// console.log('final blank: ', countAnimals());
+// console.log('final uma esp: ', countAnimals({ specie: 'penguins' }));
+
 module.exports = countAnimals;
+
+// especie || especie + sexo
+// const { especie, sexo } = animal;
+// if (sexo === undefined) {
+//   const umObj = species.filter((elObj) => elObj.name === especie);
+//   console.log(umObj);
+//   console.log(typeof umObj);
+
+//   const { residents } = umObj[0];
+//   console.log(residents);
+
+//   return residents.length;
+// const seekAnimal = species.filter((elementAnimal) => elementAnimal.name === animal);
+// const { residents } = seekAnimal[0]; // direto do req 2
+// }
