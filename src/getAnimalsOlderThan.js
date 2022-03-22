9036ec45e711a889a167ed9c1879974ab4743160
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-03-21 21:21:06 -0300

    req 2 - a insonia compensa!

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,21 +2,16 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const { name } = data.species;
+  const { species } = data;
   // find(animal)
-  const tagAnimal = name.find((elementoFind) => elementoFind === animal);
-  console.log('tagged animal:', tagAnimal);
+  const seekAnimal = species.filter((elementAnimal) => elementAnimal.name === animal);
+  // console.log(seekAnimal);
   // every (idade >= age)
-  const ageCheck = tagAnimal.every((verifier) => tagAnimal.residents.age >= age);
-  console.log('age check:', ageCheck);
-  return ageCheck;
+  const { residents } = seekAnimal[0];
+  // console.log(residents);
+  const checkAge = residents.every((elementAge) => elementAge.age >= age);
+  return checkAge;
 }
-console.log(getAnimalsOlderThan('lions', 13));
+// console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
-
-// Esta função, a partir do nome de uma espécie e uma idade mínima, verifica se todos os animais daquela espécie possuem a idade mínima especificada.
-// Observações técnicas
-// - Deve retornar um valor booleano.
-// O que será avaliado
-// - Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada.
