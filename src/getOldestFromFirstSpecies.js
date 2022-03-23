12853b7154ccf3025fcf84d8bee0a097f1dd7a55
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-03-23 05:02:32 -0300

    req 9 - agora essa parada vai

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,19 +1,26 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(...id) {
+function getOldestFromFirstSpecies(id) {
   // seu código aqui
-  // const { species } = data;
-  // const firstSpecies = id.reduce((acc, cur) => {
-  //   acc += species.find((elementoFind) => elementoFind === cur[id]);
-  //   return acc;
-  // }, []);
-  // console.log(firstSpecies);
-  // return firstSpecies;
+  const { species, employees } = data;
+  // busca func pela id
+  const getWrkr = employees.filter((eltWrkr) => eltWrkr.id === id);
+  // console.log('worker', getWrkr); // retorna objetao employee
+  // busca 1a especie tratada
+  const firstSpecies = getWrkr[0].responsibleFor[0];
+  // console.log('first speceies', firstSpecies); // first speceies 0938aa23-f153-4937-9f88-4858b24d6bce
+  // acessa animais pela id
+  const getAnimalById = species.filter((elId) => elId.id === firstSpecies);
+  // console.log(getAnimalById[0].residents); // arr de obj animais
+  // busca animal mais velho
+  const olderAnimal = getAnimalById[0].residents.sort((a, b) => b.age - a.age)[0];
+  // console.log(olderAnimal);
+  return Object.values(olderAnimal);
 }
 
 // const teste1 = 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1';
 // const teste2 = '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae';
-// console.log(getOldestFromFirstSpecies(teste1, teste2));
+// console.log('final', getOldestFromFirstSpecies(teste1));
 
 module.exports = getOldestFromFirstSpecies;
 
@@ -40,3 +47,19 @@ module.exports = getOldestFromFirstSpecies;
 //     ]
 //   },
 // ]
+// const { species } = data;
+// const firstSpecies = id.reduce((acc, cur) => {
+//   acc += species.find((elementoFind) => elementoFind === cur[id]);
+//   return acc;
+// }, []);
+// console.log(firstSpecies);
+// return firstSpecies;
+
+// const oldest = arrResids.reduce((a, b) => {
+//   if (b.age >= a.age) {
+//     a.name = b.name;
+//     a.sex = b.sex;
+//     a.age = b.age;
+//     return a;
+//   }
+// }, {});
